27954c16855a03f3991e4bc71affbc32acae4f47
Peter Berki <pberki1986@gmail.com>
2015-10-16 10:43:28 -0700

    Passing array through Average available

--- python_explorations.py
+++ python_explorations.py
@@ -1,3 +1,7 @@
+from datetime import date
+from datetime import time
+from datetime import datetime
+
 def average(x):
 	p = 0
 	if(type(x) is list):
@@ -8,4 +12,13 @@ def average(x):
 array = [23,24,25,29,30,10,5]
 print average(array)
 
+def dateTest():
+	now = datetime.now()
+	now = now.strftime("%A")
+	return now
+print dateTest()
 
+#prints all datetime
+import datetime
+everything = dir(datetime)
+print everything
